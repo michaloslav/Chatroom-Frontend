2f0768b856320608ebbe797b7925b53b2438da05
michaloslav <31165844+michaloslav@users.noreply.github.com>
2019-07-14 17:59:08 +0200

    deploy

---
+++ precache-manifest.f28d29e77653a7e4c16981164244cea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Chatroom-Frontend/robots.txt"
+  },
+  {
+    "revision": "a1acd3fcc31e67ce17bf",
+    "url": "/Chatroom-Frontend/js/chunk-vendors.4976180e.js"
+  },
+  {
+    "revision": "4bbd680ead1f97671099",
+    "url": "/Chatroom-Frontend/js/app.89f4a60d.js"
+  },
+  {
+    "revision": "b7a9d93555277b4f6c935214f1677b09",
+    "url": "/Chatroom-Frontend/index.html"
+  },
+  {
+    "revision": "4bbd680ead1f97671099",
+    "url": "/Chatroom-Frontend/css/app.f0373dc1.css"
+  }
+];
